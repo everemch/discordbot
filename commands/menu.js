e470d0ec17dc1140be3edb4a4d7986111cbac719
Edward Veremchuk <veremchuke@gmail.com>
2022-12-09 05:28:51 -0800

    Create menu slash command

---
+++ commands/menu.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('menu')
+		.setDescription('Opens the Game Menu'),
+	async execute(interaction) {
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId('Select')
+					.setPlaceholder('Select a Game')
+					.addOptions(
+						{
+							label: 'Rock Paper Scissors',
+							value: 'rps',
+						},
+						{
+							label: 'Dice Roll',
+							value: 'diceroll',
+						},
+						{
+							label: 'Coin Toss',
+							value: 'cointoss',
+						},
+					),
+			);
+
+		await interaction.reply({ content: 'Game time!', ephemeral: 'true', components: [row] });
+	},
+};
